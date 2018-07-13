fe27eb740ec04755affb9407aff2a2cf85e3bf43
ha1f <half.ldk.hiz@gmail.com>
2018-07-13 23:36:39 +0900

    show sample table

---
+++ src/components/BinaryView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="table-container">
+    <table>
+      <tr>
+        <th>address</th>
+        <th v-for="value in headers" :key="value.id">
+          {{ value }}
+        </th>
+      </tr>
+      <tr>
+        <td>
+          0000
+        </td>
+        <td v-for="value in values" :key="value.id">{{ value }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BinaryView',
+  data () {
+    return {
+      headers: Array.from(Array(10).keys()),
+      values: [10, 11, 12, 13, 14]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.table-container table {
+  margin: auto;
+}
+</style>
