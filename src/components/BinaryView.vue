7adbd442a97221e4caa0680ffa2b0328f5e989e6
ha1f <half.ldk.hiz@gmail.com>
2018-07-14 09:22:02 +0900

    Show with step, radix

--- src/components/BinaryView.vue
+++ src/components/BinaryView.vue
@@ -1,31 +1,44 @@
 <template>
   <div class="table-container">
-    <table>
+    <table border="1">
       <tr>
         <th>address</th>
-        <th v-for="j in 10" :key="j">
-          +{{ j - 1 }}
+        <th v-for="j in step" :key="j">
+          +{{ (j - 1).toString(radix) }}
         </th>
       </tr>
-      <tr v-for="i in Math.ceil(values.length / 10)" :key="i">
+      <tr v-for="i in Math.ceil(values.length / step)" :key="i">
         <td>
-          {{ (i - 1) * 10 }}
+          {{ ((i - 1) * step).toString(radix) }}
         </td>
-        <th v-for="j in 10" :key="j">
-          {{ values[(i - 1) * 10 + j - 1] }}
-        </th>
+        <td-if-present v-for="j in step" :key="j" v-bind:value="values[(i - 1) * step + j - 1]" v-bind:radix="radix"></td-if-present>
       </tr>
     </table>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+
+const tdIfPresent = Vue.extend({
+  data () {
+    return {}
+  },
+  props: [ 'value', 'radix' ],
+  template: '<td v-show="value != null">{{ (value || 0).toString(radix || 10) }}</td>'
+})
+
 export default {
   name: 'BinaryView',
   data () {
     return {
-      values: [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+      step: 16,
+      radix: 16,
+      values: Array.from(Array(100).keys())
     }
+  },
+  components: {
+    'td-if-present': tdIfPresent
   }
 }
 </script>
